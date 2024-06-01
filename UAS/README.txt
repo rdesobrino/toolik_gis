71a5eb5183efa153866d4b19079d2e7d8a6623ae
rcdesobrino <rcdesobrino@alaska.edu>
2024-05-31 16:22:32 -0800

    initial commit and edits for new Drone Harmony requirements

---
+++ UAS/README.txt
@@ -0,0 +1,28 @@
+PreflightFiles.py
+
+Rachel de Sobrino
+TFS GIS May 2024
+
+This script creates an output kml and clipped projected DEM for use in Drone Harmony software. It requires a .kml, .kmz, or .shp input file as the AOI.
+There are no required input parameters in order to make it click and run.
+
+To run:
+
+1. Right-click the script, click Open With, and navigate to the Python environment provided by ArcGIS Pro. e.g. "C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe". You can select Always if you primarily want to run scripts in the ArcGIS Pro environment to make this a click-and-go. A terminal window will pop up and close upon completion.
+-- Requires a .kml, .kmz, or .shp input file, either in the same folder as the script or paste the filepath when prompted in the terminal.
+
+OR
+
+2. Open a Python terminal using the ArcGIS Pro environment (in Start search Python and verify the filepath contains "\ArcGIS"). Type python, the path to the script, and any desired input parameters. Use -h or --help for options.
+-- all input parameters are optional. If you place the input AOI file in the same location as the script it will detect it.
+-i: path to input AOI as .shp/.kml/.kmz
+-o: output directory
+-aoi_b: size in meters to buffer the AOI (this is to ensure result of drone flight includes full AOI after processing). The default is 50m
+-d: path to DEM file. Default is PGC's ArcticDEM at 2m resolution: "Z:\Toolik\Toolik1\hypsographic\PGC_Arctic_DEM\Mosaic\ArcticDEM_mosaic.vrt"
+-dem_b: size in meters to buffer the DEM around the AOI. The default is 50m.
+-rs: resolution in meters to resample the DEM to.
+-f: desired file format. options are GeoTiff(gtif) and ASCII (ascii). Geotiff is the default. 
+
+Pay attention to the terminal. If you don't provide an AOI or the script doesn't find an AOI in its folder it will ask you for a filepath. Error handling is printed to the terminal.
+
+The output files (a .kml of the flight AOI, buffered by 50m) and a DEM (clipped to the AOI + another 50m, in 4326 WGS 84) will be exported to an \Outputs folder, or wherever you specified. Please delete the temp folder after the script completes (lock files are maintained on .gdbs so it can't be deleted from within the script)
---
+++ UAS/uas_files.py
@@ -0,0 +1,149 @@
+'''
+This script is to executed from an ArcGIS Python Shell to generate the required files used in Drone Harmony UAS Planning by Toolik GIS
+Developed May 2024, Rachel de Sobrino
+'''
+
+import arcpy
+import os
+import sys
+import argparse
+
+if  __name__ == "__main__":
+
+    os.chdir(os.path.split(os.path.abspath(__file__))[0])
+    cwd = os.getcwd()
+
+    # Looks for kmz, kml, shp in working folder:
+    def find_aoi():
+        aoi = False
+        for file in os.listdir(cwd):
+            if file.endswith(".kmz") or file.endswith(".kml") or file.endswith(".shp"):
+                aoi = os.path.abspath(file)
+        # Asks for filepath if it can't find AOI
+        while aoi == False:
+            entry = input("Please enter a valid filepath for your AOI: ").strip("\"")
+            if os.path.isfile(entry) and entry.endswith(".shp") or entry.endswith(".kmz") or entry.endswith(".kml"):
+                aoi = entry
+        return aoi
+
+    parser = argparse.ArgumentParser(description="""Create Files for Drone Harmony PreFlight Planning""")
+    parser.add_argument("-i", help=" : KMZ/KML/SHP of target study area")
+    parser.add_argument("-o", help=" : Output directory ", default = os.path.join(cwd, "Outputs"))
+    parser.add_argument("-aoi_b", help=" : AOI buffer size (meters)", default = 50)
+    parser.add_argument("-d", help=" : dem file", default = "Z:\Toolik\Toolik1\hypsographic\PGC_Arctic_DEM\Mosaic\ArcticDEM_mosaic.vrt")
+    parser.add_argument("-dem_b", help=" : DEM buffer size (meters)", default = 50)
+    parser.add_argument("-rs", help=" : resolution in meters to resample DEM", default = 0)
+    parser.add_argument("-f", help=" : output format: ascii or gtif", default="gtif")
+    args = parser.parse_args()
+
+    aoi = args.i
+    if aoi == None:
+        aoi = find_aoi()
+    aoi_type = os.path.splitext(aoi)[1]
+
+    o_dir = args.o
+    dem = args.d
+    aoi_buffer = args.aoi_b
+    d_buffer = args.dem_b
+    res = args.rs
+    f_type = args.f
+
+    # Display input parameters
+    print("AOI filepath : " , aoi)
+    print("Output directory : " , o_dir)
+    print("Output format: ", f_type)
+    print("AOI Buffer Size : " , aoi_buffer)
+    print("DEM : " , dem)
+    print("DEM Buffer Size : " , d_buffer)
+    print("Resampling: ", res)
+    print("\n")
+
+    # Cleans up output folder for current run
+    if not os.path.exists(o_dir):
+        os.mkdir(o_dir)
+    for f in os.listdir(o_dir):
+        try:
+            os.remove(os.path.join(o_dir, f))
+        except PermissionError:
+            print("Please manually delete the Outputs directory and try again")
+            sys.exit()
+
+    #Cleans up temp folder for current run
+    temp = os.path.join(cwd, "temp")
+    if not os.path.exists(temp):
+        os.mkdir(temp)
+    for f in os.listdir(temp):
+        try:
+            os.remove(os.path.join(temp, f))
+        # the .gdb usually has a lock and has to be deleted manually
+        except PermissionError:
+            print("Please manually delete the temp directory and try again: ", temp)
+            sys.exit()
+
+    name = os.path.splitext(os.path.basename(aoi))[0].replace(" ", "")
+    gdb = os.path.join(temp, name + ".gdb")
+    d_name = os.path.splitext(os.path.basename(dem))[0]
+
+#Converts KML/KMZ to Feature Layer for arcpy functions
+if aoi_type != ".shp":
+    arcpy.conversion.KMLToLayer(aoi, temp)
+    arcpy.env.workspace = gdb
+    for fc in arcpy.ListFeatureClasses('*', '', 'Placemarks'):
+        aoi = fc
+
+def buffer_aoi(aoi, aoi_buffer, buffered_aoi):
+    dist = str(aoi_buffer) + " Meters"
+    buff_aoi_shp = arcpy.analysis.PairwiseBuffer(aoi, buffered_aoi, dist)
+    print("...Buffered AOI file")
+    return buff_aoi_shp
+
+# Clip Raster always doesn't like it if the feature class and dem are in different projections but reprojecting the full dem file takes a long time
+def check_projections(aoi, dem):
+    aoi_sr = arcpy.Describe(aoi).spatialReference.factoryCode
+    dem_sr = arcpy.Describe(dem).spatialReference.factoryCode
+    if dem_sr != aoi_sr:
+        aoi = arcpy.management.Project(aoi, os.path.join(temp, name + "_" + str(dem_sr)), dem_sr)
+    return aoi
+
+def clip_dem(shp, dem):
+    shp = check_projections(shp, dem)
+    clipped_dem = arcpy.Clip_management(dem, "0 0 0 0", os.path.join(temp, d_name + "_clip.tif"), shp, "-999", "ClippingGeometry")
+    print("...Clipped DEM to AOI")
+    clipped_dem = resample_dem(clipped_dem, res)
+    return clipped_dem
+
+def resample_dem(dem, res):
+    if arcpy.Describe(dem).spatialReference.linearUnitName == "Meter" and arcpy.Raster(dem).meanCellHeight < float(res) and arcpy.Raster(dem).meanCellWidth < float(res):
+        dem = arcpy.Resample_management(dem, os.path.join(temp, d_name[:7] + "_" + str(res)), str(res), "NEAREST")
+        print("...Resampled DEM to", res, "m resolution")
+    return dem
+def project_dem(aoi, dem):
+    dem = clip_dem(aoi, dem)
+    sr = arcpy.SpatialReference(4326)
+    dem_o = arcpy.management.ProjectRaster(dem, os.path.join(o_dir, d_name + "_" + str(d_buffer) + "m_buffered.tif"), sr)
+    print("...Reprojected DEM to WGS84: 4326")
+    if f_type == "ascii":
+        arcpy.conversion.RasterToASCII(dem_o, os.path.join(o_dir, d_name + "_" + str(d_buffer) + "m_buffered.asc"))
+        print("...Converted DEM to ASCII")
+    return dem_o
+
+def aoi_shp_to_kml(shp):
+    lyr = arcpy.management.MakeFeatureLayer(shp, os.path.join(temp, name + "_Buffered"))
+    arcpy.conversion.LayerToKML(lyr, os.path.join(o_dir, name + "_" + str(aoi_buffer) + "m" + ".kml"))
+    print("...Created .kml from shapefile")
+
+# Call functions
+buffered_aoi = os.path.join(temp, name + "_buffered")
+buffered_aoi_shp = buffer_aoi(aoi, aoi_buffer, buffered_aoi)
+
+aoi_shp_to_kml(buffered_aoi_shp)
+
+buffered_aoi_for_dem = os.path.join(temp, name + "_for_DEM")
+buffered_aoi_shp = buffer_aoi(buffered_aoi_shp, aoi_buffer, buffered_aoi_for_dem)
+
+project_dem(buffered_aoi_shp, dem)
+
+print("\n!!!Please manually delete the temp folder before your next run!!!")
+
+
+
